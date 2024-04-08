6cb22b30e1f87e5db0153728fd89094e860118ae
alfandysurya2 <alfandy.surya3@gmail.com>
2024-04-08 16:23:50 +0700

    modify app_flask.py

--- app_flask.py
+++ app_flask.py
@@ -24,8 +24,8 @@ def summarize():
     outputs = model.generate(
         input_ids,
         attention_mask=attention_mask,
-        min_length=60,
-        max_length=80,
+        min_length=20,
+        max_length=80, 
         num_beams=10,
         repetition_penalty=2.5,
         length_penalty=1.0,
